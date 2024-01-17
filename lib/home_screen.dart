5b4f46cc9113a38b651fd091d7b5125d3b8e4bc2
LordRalph123 <raphaelseyindejoseph@gmail.com>
2024-01-17 09:25:22 +0100

    merged major classes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -418,3 +418,202 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+class RedOn extends StatelessWidget {
+  const RedOn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 63,
+      height: 36,
+      decoration: BoxDecoration(
+        color: const Color(0XFFFFBABE),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(18),
+        ),
+        border: Border.all(
+          color: const Color(0XFFFF8E94),
+          width: 1.0,
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(3.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 12,
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(width: 5),
+            Text(
+              "Off",
+              style: TextStyle(
+                fontSize: 14,
+                color: Color(0XFFFF8E94),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RedOff extends StatelessWidget {
+  const RedOff({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 63,
+      height: 36,
+      decoration: BoxDecoration(
+        color: const Color(0XFFFFBABE),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(18),
+        ),
+        border: Border.all(
+          color:const Color(0XFFFF8E94),
+          width: 1.0,
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(3.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "On",
+              style: TextStyle(
+                fontSize: 11,
+                color: Color(0XFFFF8E94),
+              ),
+            ),
+            SizedBox(width: 5),
+            CircleAvatar(
+              radius: 12,
+              backgroundColor: Color(0XFFFF3642),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class GateOpen extends StatelessWidget {
+  const GateOpen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 281,
+      height: 48,
+      decoration: BoxDecoration(
+        color: const Color(0xFF024F9D),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(32),
+        ),
+        border: Border.all(color: const Color(0xFF0376EC), width: 1),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.white,
+              child: Center(
+                child: Icon(
+                  Icons.lock,
+                  color: Color(0xFF024F9D),
+                ),
+              ),
+            ),
+           const SizedBox(width: 72),
+            const Text(
+              "Open",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Container(
+              width: 12,
+              height: 12,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/open.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ), 
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GateClose extends StatelessWidget {
+  const GateClose({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 281,
+      height: 48,
+      decoration: BoxDecoration(
+        color:const Color(0xFF024F9D),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(32),
+        ),
+        border: Border.all(color: const Color(0xFF0376EC), width: 1),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 100),
+            Container(
+              width: 12,
+              height: 12,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/close.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            const Text(
+              "Close",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+              ),
+            ),
+            const SizedBox(width: 64),
+            const CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.white,
+              child: Center(
+                child: Icon(
+                  Icons.lock_open,
+                  color: Color(0xFF024F9D),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
