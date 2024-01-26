303783e953f5aa44874d8c714612eaf17c8568f1
LordRalph123 <raphaelseyindejoseph@gmail.com>
2024-01-26 18:40:44 +0100

    value

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -261,7 +261,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             });
                           },
                           child: isParlor ? const RedOff() : const RedOn(),
-                          //child: ? RedOn() : RedOff(),
+                          ///child: ? RedOn() : RedOff(),
                         ),
                       ],
                     ),
