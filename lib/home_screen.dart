fdfd4fba94926484793896e92a162803d35c18d6
LordRalph123 <raphaelseyindejoseph@gmail.com>
2024-02-08 20:40:52 +0100

    =>

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -144,7 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         });
                       },
                       child:
-                          isButtonClick ? const GateClose() : constGateOpen(),
+                          isButtonClick ? const GateClose() : const GateOpen(),
                     ),
                   ],
                 ),
